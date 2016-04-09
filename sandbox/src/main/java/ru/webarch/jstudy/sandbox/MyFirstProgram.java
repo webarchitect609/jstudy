d54a6983c1ce1c564e7cef3ae20f951af66d8f49
Sergey Gripinskiy <gripinskiy@adv.ru>
2016-04-09 16:26:21 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/webarch/jstudy/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/webarch/jstudy/sandbox/MyFirstProgram.java
@@ -4,8 +4,30 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    System.out.println("The World is yours!");
+    hello("World");
+    hello("Water");
+    hello("Fire");
 
+    double s = 5;
+    System.out.println("Площадь квадрата со стороной " + s + " равна " + area(s));
+
+    double a = 4, b = 9;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+
+
+  }
+
+  public static void hello(String world) {
+    System.out.println("The " + world + " is yours!");
+  }
+
+
+  public static double area(double side) {
+    return side * side;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
