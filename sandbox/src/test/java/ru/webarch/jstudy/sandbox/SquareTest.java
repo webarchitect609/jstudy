f0a42bcdea8b8d5c36a27042d2fd33e5153d3884
Sergey Gripinskiy <gripinskiy@adv.ru>
2016-04-16 11:13:05 +0300

    Повторил SquareTest

---
+++ sandbox/src/test/java/ru/webarch/jstudy/sandbox/SquareTest.java
@@ -0,0 +1,18 @@
+package ru.webarch.jstudy.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by gripinskiy on 16.04.16.
+ */
+public class SquareTest {
+
+    @Test
+    public void testArea() {
+
+        Square sq = new Square(4);
+        Assert.assertEquals(sq.area(), 16.0);
+
+    }
+}
