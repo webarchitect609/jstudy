ba2842cf90bedd9f75ac3a8846618d7483bb70af
Sergey Gripinskiy <gripinskiy@adv.ru>
2016-04-30 12:28:04 +0300

    Коллекции: наборы элементов

---
+++ sandbox/src/main/java/ru/webarch/jstudy/sandbox/Collection.java
@@ -0,0 +1,33 @@
+package ru.webarch.jstudy.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collection {
+
+    public static void main(String[] args) {
+
+        String[] arrayOfLangs = {"Java", "C#", "Python", "PHP"};
+//
+//        for (int i = 0; i < arrayOfLangs.length; i++) {
+//            System.out.println("Я хочу выучить " + arrayOfLangs[i]);
+//        }
+
+//        List<String> listOfLangs = new ArrayList<String>();
+//        listOfLangs.add("Java");
+//        listOfLangs.add("PHP");
+
+        List<String> listOfLangs = Arrays.asList(arrayOfLangs);
+
+//        for (int i = 0; i < listOfLangs.size(); i++) {
+//            System.out.println("Я хочу выучить " + listOfLangs.get(i));
+//        }
+
+        for (String lang : listOfLangs) {
+            System.out.println("Я хочу выучить " + lang);
+        }
+        
+    }
+
+}
