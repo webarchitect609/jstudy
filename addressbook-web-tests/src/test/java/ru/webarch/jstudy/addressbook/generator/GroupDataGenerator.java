110f7b81fe54487c492756733880d1c596c73d2f
Sergey Gripinskiy <gripinskiy@adv.ru>
2016-05-14 18:42:35 +0300

    jCommander подключён к генератору

--- addressbook-web-tests/src/test/java/ru/webarch/jstudy/addressbook/generator/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/webarch/jstudy/addressbook/generator/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.webarch.jstudy.addressbook.generator;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.webarch.jstudy.addressbook.model.GroupData;
 
 import java.io.File;
@@ -11,15 +14,33 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File groupsFile = new File(args[1]);
+    @Parameter( names = {"-c", "--count"}, description = "Group count to generate", required = true)
+    private int count;
 
-        List<GroupData> groupDataList = generate(count);
-        save(groupDataList, groupsFile);
+    @Parameter( names = {"-f", "--file"}, description = "File name to save csv group data", required = true)
+    private String file;
+
+    public static void main(String[] args) throws IOException {
+
+        GroupDataGenerator groupDataGenerator = new GroupDataGenerator();
+        JCommander jCommander = new JCommander(groupDataGenerator);
+        try {
+            jCommander.parse(args);
+            groupDataGenerator.run();
+        } catch (ParameterException e) {
+            jCommander.usage();
+        }
     }
 
-    private static List<GroupData> generate(int count) {
+    private void run() throws IOException {
+        List<GroupData> groupDataList = generate(count);
+        File groupsFile = new File(file);
+        save(groupDataList, groupsFile);
+        System.out.println("Groups generated: " + count);
+        System.out.println("Saved to: " + groupsFile.getPath());
+    }
+
+    private List<GroupData> generate(int count) {
         List<GroupData> groupDataList = new ArrayList<>();
 
         for (int i = 1; i <= count; i++) {
@@ -34,7 +55,7 @@ public class GroupDataGenerator {
         return groupDataList;
     }
 
-    private static void save(List<GroupData> groupDataList, File groupsFile) throws IOException {
+    private void save(List<GroupData> groupDataList, File groupsFile) throws IOException {
         try (Writer csv = new FileWriter(groupsFile)) {
             for (GroupData group : groupDataList) {
                 csv.write(String.format("%s;%s;%s\r\n", group.getName(), group.getHeader(), group.getFooter()));
