7541fb144983c0731df428dc628f46aa88a18380
Sergey Gripinskiy <gripinskiy@adv.ru>
2016-06-07 16:48:40 +0300

    REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/webarch/jstudy/github/tests/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.webarch.jstudy.github.tests;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests  {
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("8cb8a484317ba8fbc9c87bdbd25962f9750d3540");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("webarchitect609", "jstudy")).commits();
+
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
