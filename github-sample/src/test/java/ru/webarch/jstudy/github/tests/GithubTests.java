ba4ef3f73ece51ea0f6a1f36a37b1f8a182a8221
Sergey Gripinskiy <gripinskiy@adv.ru>
2016-06-13 18:14:39 +0300

    Добавлено замечание: github-sample не работает

--- github-sample/src/test/java/ru/webarch/jstudy/github/tests/GithubTests.java
+++ github-sample/src/test/java/ru/webarch/jstudy/github/tests/GithubTests.java
@@ -9,6 +9,9 @@ import java.io.IOException;
 public class GithubTests  {
     @Test
     public void testCommits() throws IOException {
+        /**
+         * Тест больше работать не будет, т.к. GitHub автоматически отозвал этот токен, как потенциальную опасность
+         */
         Github github = new RtGithub("8cb8a484317ba8fbc9c87bdbd25962f9750d3540");
         RepoCommits commits = github.repos().get(new Coordinates.Simple("webarchitect609", "jstudy")).commits();
 
